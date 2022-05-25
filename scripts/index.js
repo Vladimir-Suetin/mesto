e2c839ca8ab0afe147e29e0e42b0a3ff379a124f
Vladimir-NB <suetin-vladimir@yandex.ru>
2022-05-25 20:33:48 +0300

    feat: завершить создание файла README

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,25 @@
 // Находим профиль в DOM
-let profile = document.querySelector(".profile");
+let profile = document.querySelector('.profile');
 // Находим поля профиля в DOM
-let profileName = profile.querySelector(".profile__name");
-let profilejob = profile.querySelector(".profile__job");
+let profileName = profile.querySelector('.profile__name');
+let profilejob = profile.querySelector('.profile__job');
 
 // Находим форму в DOM
-let formElement = document.querySelector(".popup"); // Воспользуйтесь методом querySelector()
+let formElement = document.querySelector('.popup'); // Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
-let nameInput = formElement.querySelector(".popup__name"); // Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector(".popup__job"); // Воспользуйтесь инструментом .querySelector()
+let nameInput = formElement.querySelector('.popup__name'); // Воспользуйтесь инструментом .querySelector()
+let jobInput = formElement.querySelector('.popup__job'); // Воспользуйтесь инструментом .querySelector()
 
 // Находим кнопку редактирования формы
-let profileEditButton = document.querySelector(".profile__edit-button");
+let profileEditButton = document.querySelector('.profile__edit-button');
 //создаем функцию открытия popup
 function popupOpened() {
-  formElement.classList.toggle("popup_opened");
+  formElement.classList.toggle('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profilejob.textContent;
 }
 //вызываем функцию при прослушивании click
-profileEditButton.addEventListener("click", popupOpened);
+profileEditButton.addEventListener('click', popupOpened);
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -34,13 +34,13 @@ profilejob.textContent = jobInput.value;
 
 // Прикрепляем обработчик к форме:
 // Находим кнопку закрытия popup
-let closeButton = document.querySelector(".popup__close-icon");
+let closeButton = document.querySelector('.popup__close-icon');
 //создаем функцию закрытия
 function closeFormButton() {
-  formElement.classList.remove("popup_opened");
+  formElement.classList.remove('popup_opened');
 }
 //вызываем функцию при прослушивании click
-closeButton.addEventListener("click", closeFormButton);
+closeButton.addEventListener('click', closeFormButton);
 
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);

232642cdac743cb50f3380562cd5c4391f498daf
Vladimir-NB <suetin-vladimir@yandex.ru>
2022-05-29 23:38:02 +0300

    выполнение рекомендаций ревью

--- scripts/index.js
+++ scripts/index.js
@@ -15,9 +15,9 @@ let jobInput = formElement.querySelector(".popup__job"); // Воспользуй
 // Находим кнопку редактирования формы
 let profileEditButton = document.querySelector(".profile__edit-button");
 
-// Прикрепляем обработчик к форме:
 // Находим кнопку закрытия popup
 let closeButton = document.querySelector(".popup__close-icon");
+//создаем функцию закрытия
 
 //создаем функцию открытия popup
 function openPopup() {
@@ -26,23 +26,24 @@ function openPopup() {
   jobInput.value = profilejob.textContent;
 }
 
+//создаем функцию закрытия popup
+function closePopup() {
+  formElement.classList.remove("popup_opened");
+}
+
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+
   // Так мы можем определить свою логику отправки.
   // О том, как это делать, расскажем позже.
+
   // Вставьте новые значения с помощью textContent
   profileName.textContent = nameInput.value;
   profilejob.textContent = jobInput.value;
 
-  // при прослушивании события submit вызывает функцию закрытия
   formElement.addEventListener("submit", closePopup);
-}
-
-//создаем функцию закрытия
-function closePopup() {
-  formElement.classList.remove("popup_opened");
 }
 
 //вызываем функцию при прослушивании click
@@ -50,3 +51,6 @@ closeButton.addEventListener("click", closePopup);
 
 //вызываем функцию при прослушивании click
 profileEditButton.addEventListener("click", openPopup);
+
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener("submit", formSubmitHandler);

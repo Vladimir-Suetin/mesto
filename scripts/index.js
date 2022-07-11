1c120e4aabb9ada8c67d7e0649747e7f16e8765d
Suetin_Vladimir <Suetin-vladimir@yandex.ru>
2022-07-11 15:06:21 +0300

    исправить и расроложить элементы popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,22 @@
-// Находим профиль в DOM
-let profile = document.querySelector(".profile");
-
-// Находим поля профиля в DOM
-let profileName = profile.querySelector(".profile__name");
-let profilejob = profile.querySelector(".profile__job");
-
-// Находим форму в DOM
-let formElement = document.querySelector(".popup"); // Воспользуйтесь методом querySelector()
-
-// Находим поля формы в DOM
-let nameInput = formElement.querySelector(".popup__name"); // Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector(".popup__job"); // Воспользуйтесь инструментом .querySelector()
-
+// Находим профиль
+const profile = document.querySelector(".profile");
+// Находим поля профиля
+const profileName = profile.querySelector(".profile__name");
+const profilejob = profile.querySelector(".profile__job");
 // Находим кнопку редактирования формы
-let profileEditButton = document.querySelector(".profile__edit-button");
-
+const profileEditButton = profile.querySelector(".profile__edit-button");
+// Находим форму
+const formElement = document.querySelector(".popup");
+// Находим поля формы
+const nameInput = formElement.querySelector(".popup__name");
+const jobInput = formElement.querySelector(".popup__job");
 // Находим кнопку закрытия popup
-let closeButton = document.querySelector(".popup__close-icon");
-//создаем функцию закрытия
+const closeButton = formElement.querySelector(".popup__close-icon");
+
 
 //создаем функцию открытия popup
 function openPopup() {
-  formElement.classList.toggle("popup_opened");
+  formElement.classList.add("popup_opened");
   nameInput.value = profileName.textContent;
   jobInput.value = profilejob.textContent;
 }
@@ -36,20 +31,17 @@ function closePopup() {
 function formSubmitHandler(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
 
-  // Так мы можем определить свою логику отправки.
-  // О том, как это делать, расскажем позже.
-
-  // Вставьте новые значения с помощью textContent
+  // Вставляем новые значения с помощью textContent
   profileName.textContent = nameInput.value;
   profilejob.textContent = jobInput.value;
 
   closePopup()
 }
 
-//вызываем функцию при прослушивании click
+//вызываем функцию закрытия при прослушивании click
 closeButton.addEventListener("click", closePopup);
 
-//вызываем функцию при прослушивании click
+//вызываем функцию открытия при прослушивании click
 profileEditButton.addEventListener("click", openPopup);
 
 // он будет следить за событием “submit” - «отправка»
